cd8af81e1ac73b5ee2c83aa2f2613cb4f3be339b
Serinadayo <144571368+Serinadayo@users.noreply.github.com>
2023-11-27 22:29:11 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+    webhookURL: 'ここにウェブフックURL',
+    messageContent: '送信するメッセージ',
+    sendMessageInterval: 100, //送信間隔（ミリ秒）
+    sendMessageCount: 10
+};
+
+module.exports = config;
---
+++ main.js
@@ -0,0 +1,28 @@
+const { WebhookClient } = require('discord.js');
+
+// Webhook Client
+const webhookClient = new WebhookClient({ url: webhookURL });
+
+// Message Content
+const messageContent = 'test';
+
+// Message Send
+function sendMessage() {
+    webhookClient.send(config.messageContent)
+    .then(() => console.log('message sent successfully!'))
+    .catch(error => console.error('Error sending message:', error));
+}
+
+sendMessage();
+
+//時間・回数指定によるメッセージ送信
+let sentCount = 1;
+const interval = setInterval(() => {
+    if(sentCount < config.sendMessageCount) {
+        sendMessage();
+        sentCount++;
+    }else {
+        clearInterval(interval);
+        webhookClient.destroy();
+    }
+}, config.sendMessageInterval); //0.1秒間
